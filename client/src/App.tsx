a2c84a9c7a4660c7948c1976f42aed331fce187a
NikolozKapanadzee <nkapanadze369@gmail.com>
2026-01-29 07:54:02 +0400

    feat: installed react router,created components and pages folders

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,20 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router";
 import "./index.css";
+import Register from "./(pages)/register/Register";
+import Login from "./(pages)/login/Login";
+import Dashboard from "./(pages)/dashboard/Dashboard";
 
 function App() {
   return (
     <>
-      <h1 className="text-amber-400">App.tsx</h1>
+      <Router>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="*" element={<h1>404 Page Not Found</h1>} />
+        </Routes>
+      </Router>
     </>
   );
 }
